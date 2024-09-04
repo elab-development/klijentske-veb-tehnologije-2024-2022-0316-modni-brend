1ff606771aa367409240910b53750ff89e63e767
Veljko Knezevic <veljkoknez11@gmail.com>
2024-09-04 23:40:47 +0200

    Proveravanje da li se ispunjavaju taskovi 1-8

--- src/app/features/Brand/dashboard/ProductList.tsx
+++ src/app/features/Brand/dashboard/ProductList.tsx
@@ -6,20 +6,22 @@ const ProductList = () => {
     const [products, setProducts] = useState<Product[]>([]);
 
     const addProduct = () => {
-        const newProduct = new Product(1, "Example Product", 99.99, "Clothing");
+        const newProduct = new Product(1, "Blue college jacket", 99.99, "Clothing");
         setProducts([...products, newProduct]);
     };
 
     return (
         <div>
-            <h1>Products</h1>
-            <button onClick={addProduct}>Add Product</button>
+            <h1>Here you can win random product from our warehouse </h1>
+            <button onClick={addProduct} style={{padding: '0.5rem 2rem'}}>Roll</button>
             <ul>
+                
                 {products.map(prod => (
-                    <li key={prod.id}>
-                        <h2>{prod.name}</h2>
-                        <p>Price: ${prod.price}</p>
-                        <p>Category: {prod.category}</p>
+                    <li style={{listStyle: 'none',marginTop: '2rem'}} key={prod.id}>
+                        <h1 style={{marginTop: '5rem'}}>You won</h1>
+                        <h2 style={{color: 'black'}}>{prod.name}</h2>
+                        <p style={{color: 'black'}}>Price: ${prod.price}</p>
+                        <p style={{color: 'black'}}>Category: {prod.category}</p>
                     </li>
                 ))}
             </ul>
